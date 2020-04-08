67d442cc1d975b2a746181c7d7b5e191a7bef3c3
Carlos Rhedney <twisterpsa@hotmail.com>
2020-04-08 16:15:42 -0300

    Add Classe-Objeto

---
+++ ClasseObjeto/src/com/projeto/classeobjeto/Casa.java
@@ -0,0 +1,14 @@
+package com.projeto.classeobjeto;
+
+public class Casa {
+	/**
+	 * @autor Carlos Rhedney
+	 */
+	String cor;
+	int vagasGaragem;
+	
+	public void abrirGaragem() {
+		System.out.println("A garagem foi aberta!");
+	}
+
+}
---
+++ ClasseObjeto/src/com/projeto/classeobjeto/ClasseObjeto.java
@@ -0,0 +1,16 @@
+package com.projeto.classeobjeto;
+
+public class ClasseObjeto {
+	/**
+	 * @autor Carlos Rhedney
+	 */
+
+	public static void main(String[] args) {
+		Casa casa = new Casa();
+		casa.cor = "Vermelho";
+		System.out.println(casa.cor);
+		casa.abrirGaragem();
+
+	}
+
+}
